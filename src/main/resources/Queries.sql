873ced8da3fe77fb85cefd2653365a3b492ffca5
B Pavan Kumar <pavan.b9@gmail.com>
2023-09-11 18:11:33 +0530

    Introduced a placeholder to safeguard all the queries used, for setup purposes

---
+++ src/main/resources/Queries.sql
@@ -0,0 +1,13 @@
+-- Main Expense categories
+INSERT INTO expense VALUES (1, 'Expenses made at any restaurant, cafe or any other eatery as such', null, 'Food & Beverage');
+INSERT INTO expense VALUES (2, 'Expenses made on a monthly basis such as bills', null, 'Bills & Utilities');
+INSERT INTO expense VALUES (3, 'Expenses made towards transportation, travel', null, 'Transportation');
+INSERT INTO expense VALUES (4, 'Expenses made as part of any purchases, shopping', null, 'Shopping');
+INSERT INTO expense VALUES (5, 'Expenses made at any Movies, Events, Functions', null, 'Entertainment');
+INSERT INTO expense VALUES (6, 'Expenses made in order to maintain Family wellness, grooming & medical expenses', null, 'Health & Fitness');
+INSERT INTO expense VALUES (7, 'Expenses made as part of any functions, marriages etc. as Gifts', null, 'Gifts & Donation');
+INSERT INTO expense VALUES (8, 'Expenses made as part of any renovation, specific to house or family', null, 'Family');
+INSERT INTO expense VALUES (9, 'Expenses made as part of Childrens Education or self-improvement', null, 'Education');
+INSERT INTO expense VALUES (10, 'Expenses made as part of Investments - usually FDs & RDs', null, 'Investment');
+INSERT INTO expense VALUES (11, 'Expenses made as part of LIC policy premiums for Self, Spouse & Kids', null, 'Insurances');
+INSERT INTO expense VALUES (12, 'Expenses made which do not fit the above categories', null, 'Other Expenses');
