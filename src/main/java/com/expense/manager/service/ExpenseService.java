da7f446e62d81f9cbd416b1efea14820084ea9ee
B Pavan Kumar <pavan.b9@gmail.com>
2023-09-17 09:24:04 +0530

    Corrected the deployment issue with the introduced Service class

--- src/main/java/com/expense/manager/service/ExpenseService.java
+++ src/main/java/com/expense/manager/service/ExpenseService.java
@@ -3,11 +3,12 @@ package com.expense.manager.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.stereotype.Service;
 
 import com.expense.manager.entity.Expense;
 import com.expense.manager.repository.ExpenseRepository;
 
+@Service
 public class ExpenseService {
 
 	@Autowired
@@ -15,14 +16,5 @@ public class ExpenseService {
 
 	public List<Expense> getAllExpenses() {
 		return null;
-	}
-
-	public List<Expense> getAllTopLevelExpenses() {
-		// Assuming top-level expenses have null expenseCategoryId
-		return expenseRepository.findByExpenseCategoryId(null);
-	}
-
-	public List<Expense> getExpensesByCategory(@PathVariable Long id) {
-		return expenseRepository.findByExpenseCategoryId(id);
 	}
 }
